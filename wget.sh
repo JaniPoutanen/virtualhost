32fddf77b4c16b6a51e0ed47597b135aa538b633
Jani Poutanen <janipoutanen@gmail.com>
2017-04-17 17:00:59 +0000

    Update wget.sh

--- wget.sh
+++ wget.sh
@@ -1,3 +1,3 @@
 #from https://unix.stackexchange.com/questions/228412/how-to-wget-a-github-file
 
-wget -O start.sh https://github.com/JaniPoutanen/virtualhost/blob/master/start.sh
+wget start.sh https://github.com/JaniPoutanen/virtualhost/blob/master/start.sh
