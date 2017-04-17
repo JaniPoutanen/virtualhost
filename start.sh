5c80d50391134299f160a91e9612b24369a59f5e
Jani Poutanen <janipoutanen@gmail.com>
2017-04-17 14:18:22 +0000

    Add modules

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 setxkbmap fi
 sudo apt-get update
 sudo apt-get -y install puppet git
-git clone https://github.com/janipoutanen/virtualhost.git
+git clone https://github.com/JaniPoutanen/virtualhost.git
 cd start
 bash apply.sh
