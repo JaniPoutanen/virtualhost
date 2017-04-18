bfcf7bc436f62af1bca92da0295c4493e02bfd19
jani <janipoutanen@gmail.com>
2017-04-18 06:44:40 +0000

    update init.pp

--- modules/virtualhost/manifests/init.pp
+++ modules/virtualhost/manifests/init.pp
@@ -12,26 +12,25 @@ class virtualhost {
 		require => Service['apache2'],
 	}
 	file { '/home/xubuntu/public_html':
-        	ensure => 'directory',
+		ensure => 'directory',
         }    
-    	file { '/home/xubuntu/public_html/index.html':
-        	content => template('virtualhost/index.html.erb'),
-        	require => File['/home/xubuntu/public_html'],
+	file { '/home/xubuntu/public_html/index.html':
+		content => template('virtualhost/index.html.erb'),
+		require => File['/home/xubuntu/public_html'],
 	}
 	service { 'apache2':
 		ensure => 'true',
 		enable => 'true',
 		provider => 'systemd',
 	}
-	exec { 'a2ensite':
+	exec { 'virtualhost':
 		command => 'sudo a2ensite oliot.conf',      
                 path => '/bin:/usr/bin:/sbin:/usr/sbin:',
 		require => File['/etc/apache2/sites-available/oliot.conf'],		
         }
-	exec { 'a2dissite':
+	exec { 'virtualhost':
                 command => 'sudo a2dissite 000-default.conf',
                 path => '/bin:/usr/bin:/sbin:/usr/sbin:',
-		require => File['/etc/apache2/sites-available/oliot.conf'],		
 		notify => Service['apache2'],
 	}
 }
